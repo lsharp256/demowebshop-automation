de95f829d241d559bf954659564e63832b643ecf
Leroy <lsharp256@gmail.com>
2025-04-08 00:50:59 +0200

    Updated BaseTest

--- src/test/java/com/demo/qa/tests/BaseTest.java
+++ src/test/java/com/demo/qa/tests/BaseTest.java
@@ -3,16 +3,28 @@ package com.demo.qa.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
+    public void setup() throws IOException {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Create a unique user data directory for each test session
+        Path tempDir = Files.createTempDirectory("webdriver-user-data");
+        options.addArguments("user-data-dir=" + tempDir.toAbsolutePath().toString());
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://demowebshop.tricentis.com/");
     }
